c231827ba30fd298d55f1c87ff63ad8b33eaa6f3
john xu <john@taiko.xyz>
2025-02-19 14:40:58 +0800

    Refactor JSON unmarshaling in MptNode to use consistent variable naming for improved clarity

--- internal/mpt/mpt_marshaling.go
+++ internal/mpt/mpt_marshaling.go
@@ -16,40 +16,40 @@ func (m *MptNode) UnmarshalJSON(data []byte) error {
 	for key, val := range raw {
 		switch key {
 		case "Branch":
-			var inner BranchNode
-			if err := json.Unmarshal(val, &inner); err != nil {
+			var data BranchNode
+			if err := json.Unmarshal(val, &data); err != nil {
 				return err
 			}
-			m.Data = &inner
+			m.Data = &data
 		case "Leaf":
-			var inner [2]json.RawMessage
-			if err := json.Unmarshal(val, &inner); err != nil {
+			var data [2]json.RawMessage
+			if err := json.Unmarshal(val, &data); err != nil {
 				return err
 			}
 			m.Data = &LeafNode{
-				Prefix: inner[0],
-				Value:  inner[1],
+				Prefix: data[0],
+				Value:  data[1],
 			}
 		case "Extension":
-			var inner [2]json.RawMessage
-			if err := json.Unmarshal(val, &inner); err != nil {
+			var data [2]json.RawMessage
+			if err := json.Unmarshal(val, &data); err != nil {
 				return err
 			}
 			var child MptNode
-			if err := json.Unmarshal(inner[1], &child); err != nil {
+			if err := json.Unmarshal(data[1], &child); err != nil {
 				return err
 			}
 			ext := &ExtensionNode{
-				Prefix: inner[0],
+				Prefix: data[0],
 				Child:  &child,
 			}
 			m.Data = ext
 		case "Digest":
-			var inner DigestNode
-			if err := json.Unmarshal(val, &inner); err != nil {
+			var data DigestNode
+			if err := json.Unmarshal(val, &data); err != nil {
 				return err
 			}
-			m.Data = &inner
+			m.Data = &data
 		case "Null":
 			m.Data = &NullNode{}
 		default:
