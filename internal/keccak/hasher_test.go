aa7ae331dc6e39c66b7149846d8862617c034eac
john xu <john@taiko.xyz>
2025-02-28 23:29:39 +0800

    refactor: simplify assertion in TestEmpty for improved readability

--- internal/keccak/hasher_test.go
+++ internal/keccak/hasher_test.go
@@ -10,8 +10,7 @@ import (
 func TestEmpty(t *testing.T) {
 	assert.Equal(
 		t,
-		common.HexToHash("c5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470").
-			Bytes(),
+		common.HexToHash("c5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470"),
 		Keccak([]byte{}),
 	)
 }
