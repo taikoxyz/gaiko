54f8903623f459a6df5a4c0e8102a5a2aee62703
john xu <john@taiko.xyz>
2025-05-07 13:27:12 +0800

    refactor: update Response struct and improve proveHandler logic

--- cmd/gaiko/server.go
+++ cmd/gaiko/server.go
@@ -3,13 +3,12 @@ package main
 import (
 	"bytes"
 	"context"
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
-
-	json "github.com/goccy/go-json"
 
 	"github.com/taikoxyz/gaiko/internal/flags"
 	"github.com/taikoxyz/gaiko/internal/prover"
@@ -22,21 +21,22 @@ type ProveData struct {
 }
 
 type Response struct {
-	Status  string               `json:"status"`
-	Message string               `json:"message"`
-	Proof   prover.ProofResponse `json:"proof"`
+	Status  string          `json:"status"`
+	Message string          `json:"message"`
+	Proof   json.RawMessage `json:"proof"`
 }
 
 type ProveMode int
 
 const (
-	OntakeBlock   ProveMode = iota // 0
-	PacayaBatch                    // 1
-	Aggregation                    // 2
-	Bootstrap                      // 3
-	StatusCheck                    // 4
-	TestHeartBeat                  // 5
-	HeklaBlock                     // 6 deprecated
+	Unknown       ProveMode = iota // 0
+	OntakeBlock                    // 1
+	PacayaBatch                    // 2
+	Aggregation                    // 3
+	Bootstrap                      // 4
+	StatusCheck                    // 5
+	TestHeartBeat                  // 6
+	HeklaBlock                     // 7 deprecated
 )
 
 func proveHandler(ctx context.Context, args *flags.Arguments, sgxProver *prover.SGXProver, w http.ResponseWriter, r *http.Request, proveMode ProveMode) {
@@ -45,11 +45,6 @@ func proveHandler(ctx context.Context, args *flags.Arguments, sgxProver *prover.
 		return
 	}
 
-	// body, err := io.ReadAll(r.Body)
-	// if err != nil {
-	// 	http.Error(w, "Read request failed", http.StatusBadRequest)
-	// 	return
-	// }
 	defer r.Body.Close()
 	contentType := r.Header.Get("Content-Type")
 	if contentType != "application/json" {
@@ -60,22 +55,22 @@ func proveHandler(ctx context.Context, args *flags.Arguments, sgxProver *prover.
 
 	// call different command according to data.Type
 	var err error
-	var proofResponse prover.ProofResponse
+	var proofResponse []byte
 	switch proveMode {
 	case TestHeartBeat:
 		fmt.Fprintf(args.ProofWriter, "Hello, %s!", "world")
 	case OntakeBlock:
 		err = oneshot(ctx, sgxProver, args)
-		_ = json.Unmarshal(args.ProofWriter.(*bytes.Buffer).Bytes(), &proofResponse)
+		proofResponse = args.ProofWriter.(*bytes.Buffer).Bytes()
 	case HeklaBlock:
 		http.Error(w, "Hekla block prove is deprecated", http.StatusBadRequest)
-		err = fmt.Errorf("hekla block prove is deprecated")
+		return
 	case PacayaBatch:
 		err = batchOneshot(ctx, sgxProver, args)
-		_ = json.Unmarshal(args.ProofWriter.(*bytes.Buffer).Bytes(), &proofResponse)
+		proofResponse = args.ProofWriter.(*bytes.Buffer).Bytes()
 	case Aggregation:
 		err = aggregate(ctx, sgxProver, args)
-		_ = json.Unmarshal(args.ProofWriter.(*bytes.Buffer).Bytes(), &proofResponse)
+		proofResponse = args.ProofWriter.(*bytes.Buffer).Bytes()
 	case Bootstrap:
 		err = bootstrap(ctx, sgxProver, args)
 	case StatusCheck:
@@ -87,11 +82,11 @@ func proveHandler(ctx context.Context, args *flags.Arguments, sgxProver *prover.
 
 	var response Response
 	if err != nil {
-		fmt.Println("Prove finished, get error: ", err.Error())
+		fmt.Printf("Prove finished, get error: %s, response: %s\n", err.Error(), string(proofResponse))
 		response = Response{
 			Status:  "error",
 			Message: err.Error(),
-			Proof:   prover.NewDefaultProofResponse(),
+			Proof:   []byte("{}"),
 		}
 	} else {
 		fmt.Printf("Prove finished, get proof %s\n", proofResponse)
@@ -117,46 +112,33 @@ func runServer(c *cli.Context) error {
 	if port == "" {
 		port = "8080"
 	}
-
-	// TODO: using closet for easy test maybe not a good idea
-	http.HandleFunc("/prove/block", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/prove/{action}", func(w http.ResponseWriter, r *http.Request) {
 		args := flags.NewArguments(c)
 		// override the proof writer to get the proof & return as response
 		args.ProofWriter = new(bytes.Buffer)
 		args.WitnessReader = r.Body
 		sgxProver := prover.NewSGXProver(args)
-		proveHandler(c.Context, args, sgxProver, w, r, OntakeBlock)
-	})
-	http.HandleFunc("/prove/batch", func(w http.ResponseWriter, r *http.Request) {
-		args := flags.NewArguments(c)
-		// override the proof writer to get the proof & return as response
-		args.ProofWriter = new(bytes.Buffer)
-		args.WitnessReader = r.Body
-		sgxProver := prover.NewSGXProver(args)
-		proveHandler(c.Context, args, sgxProver, w, r, PacayaBatch)
-	})
-	http.HandleFunc("/prove/aggregate", func(w http.ResponseWriter, r *http.Request) {
-		args := flags.NewArguments(c)
-		// override the proof writer to get the proof & return as response
-		args.ProofWriter = new(bytes.Buffer)
-		args.WitnessReader = r.Body
-		sgxProver := prover.NewSGXProver(args)
-		proveHandler(c.Context, args, sgxProver, w, r, Aggregation)
-	})
-	http.HandleFunc("/check", func(w http.ResponseWriter, r *http.Request) {
-		args := flags.NewArguments(c)
-		// override the proof writer to get the proof & return as response
-		args.ProofWriter = new(bytes.Buffer)
-		args.WitnessReader = r.Body
-		sgxProver := prover.NewSGXProver(args)
-		proveHandler(c.Context, args, sgxProver, w, r, StatusCheck)
-	})
-	http.HandleFunc("/bootstrap", func(w http.ResponseWriter, r *http.Request) {
-		args := flags.NewArguments(c)
-		// override the proof writer to get the proof & return as response
-		args.BootstrapWriter = new(bytes.Buffer)
-		sgxProver := prover.NewSGXProver(args)
-		proveHandler(c.Context, args, sgxProver, w, r, Bootstrap)
+		proveMode := Unknown
+		if r.URL.Query().Get("debug") == "true" {
+			args.SGXType = "debug"
+		}
+		switch r.PathValue("action") {
+		case "block":
+			proveMode = OntakeBlock
+		case "batch":
+			proveMode = PacayaBatch
+		case "aggregate":
+			proveMode = Aggregation
+		case "bootstrap":
+			proveMode = Bootstrap
+		case "check":
+			proveMode = StatusCheck
+		case "heartbeat":
+			proveMode = TestHeartBeat
+		case "hekla":
+			proveMode = HeklaBlock
+		}
+		proveHandler(r.Context(), args, sgxProver, w, r, proveMode)
 	})
 
 	server := &http.Server{
